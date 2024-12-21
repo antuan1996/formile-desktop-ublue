5d4cb162c4ccbf09f27cebc0cb2322990340ebec
Formile <antonar47@gmail.com>
2024-12-22 01:13:38 +0300

    updated zerotier install method

--- config/scripts/zerotier.sh
+++ config/scripts/zerotier.sh
@@ -6,17 +6,18 @@
 set -oue pipefail
 
 echo 'Installing ZeroTier client'
-curl -s \
-https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg \
-| tee /etc/pki/rpm-gpg/RPM-GPG-KEY-zerotier
+# curl -s \
+# https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg \
+# | tee /etc/pki/rpm-gpg/RPM-GPG-KEY-zerotier
 
-cat > /etc/yum.repos.d/zerotier.repo<< EOF
-[zerotier]
-name=ZeroTier, Inc. RPM Release Repository
-baseurl=http://download.zerotier.com/redhat/fc/$releasever
-enabled=1
-gpgcheck=1
-EOF
+# cat > /etc/yum.repos.d/zerotier.repo<< EOF
+# [zerotier]
+# name=ZeroTier, Inc. RPM Release Repository
+# baseurl=http://download.zerotier.com/redhat/fc/$releasever
+# enabled=1
+# gpgcheck=1
+# EOF
 
+rpm-ostree install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 rpm-ostree install zerotier-one
 systemctl enable zerotier-one.service
